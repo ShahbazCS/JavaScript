c51ef12a1e8109d1fe34cb21dd8ea4fd9a1cc716
Shahbaz Khan <72274984+ShahbazCS@users.noreply.github.com>
2022-08-04 16:01:07 +0530

    Create 06_Operators.js

---
+++ 06_Operators.js
@@ -0,0 +1,33 @@
+console.log("Operators in js")
+let a=45
+let b=4
+console.log("a+b= ",a+b)
+console.log("a-b= ",a-b)
+console.log("a*b= ",a*b)
+console.log("a/b= ",a/b) //In decimal
+console.log("a**b= ",a**b)
+console.log("a%b= ",a%b)
+console.log("a++= ",a++) //45
+console.log("a= ",a) //46
+console.log("++a= ",++a) //47
+console.log("a= ",a)//47
+console.log("a--= ",a--) //47
+console.log("a= ",a) //46
+console.log("--a= ",--a) //45
+console.log("a= ",a) //45
+//Comparison Operaters
+let comp1=7;
+let comp2="6"
+console.log("comp1==comp2 is ",comp1==comp2)
+console.log("comp1!=comp2 is ",comp1!=comp2)
+//above operators work on equal absolute value
+console.log("comp1===comp2 is ",comp1===comp2)
+console.log("comp!==comp2 is ",comp1!==comp2)
+console.log("comp1>comp2 is ",comp1>comp2)
+// above two operators work on equal value and same types
+
+
+/* Multi
+Line
+Comment
+*/
