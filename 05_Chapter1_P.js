8aa3e919d46acf385f9e9d62b520da98ccce9345
Shahbaz Khan <72274984+ShahbazCS@users.noreply.github.com>
2022-08-04 14:45:12 +0530

    Create 05_Chapter1_P.js

---
+++ 05_Chapter1_P.js
@@ -0,0 +1,25 @@
+//Q1
+let a="Khan"
+let b=6
+console.log(a+b)
+//Q2
+console.log(typeof(a+b))
+//Q3
+const a1={
+  name:"Shahbaz",
+  section:"B",
+  isTopper:false
+}
+//a=45
+a1['friend']="Bisal"
+a1['name']="Bibek"
+console.log(a1)
+//a1={} will give error a1 is already refrenced to an object
+//Q5
+const dict={
+  appreciate:"recognize worth of",
+  ataraxia:"freedom from emotional disturbances",
+  yakka:"work,especially hard work"
+}
+console.log(dict.yakka)
+console.log(dict['yakka'])
