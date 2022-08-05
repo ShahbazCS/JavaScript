e628c8bfcc1539c99ae197ee86136a92f04eff98
Shahbaz Khan <72274984+ShahbazCS@users.noreply.github.com>
2022-08-05 09:52:55 +0530

    Create 10_while_loops.js

---
+++ 10_while_loops.js
@@ -0,0 +1,15 @@
+//while loop
+let n=prompt("Enter n")
+n=Number.parseInt(n)
+let i=10;
+// while(i<n){
+//   console.log(i)
+//   i++
+// }
+
+//do while
+do{
+  console.log(i)
+  i++
+}while(i<n) //if n=2 still for i=10, loop will run atleast once and print 10
+
