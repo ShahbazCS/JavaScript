00b632f1036e34c549785613f2548afacd106767
Shahbaz Khan <72274984+ShahbazCS@users.noreply.github.com>
2022-08-05 20:28:10 +0530

    Create 16_Arrays.js

---
+++ 16_Arrays.js
@@ -0,0 +1,9 @@
+let marks=[91,86,22,null,"Ghar pe moj li"]
+console.log(marks[0])
+console.log(marks[4])
+console.log("length of array",marks.length)
+marks[0]="jhoota medical lagaya"
+console.log(marks[0])
+console.log(marks[75]) // as this index doesn't exist, it prints undefined
+//array is of object type
+console.log(typeof marks)
