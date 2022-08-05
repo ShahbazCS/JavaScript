c5820f78cf325cab51a0aa8eb1a72c7740fe02e7
Shahbaz Khan <72274984+ShahbazCS@users.noreply.github.com>
2022-08-05 09:42:27 +0530

    Create 09_For_loops.js

---
+++ 09_For_loops.js
@@ -0,0 +1,27 @@
+//for loop
+// let sum=0;
+// let n=prompt("enter value of n")
+// n=Number.parseInt(n)
+// for(let i=0;i<=n;i++){
+//   sum=sum+i;
+// }
+// console.log("sum is ",sum)
+
+let obj ={
+  Bibek:98,
+  Bisal:100,
+  VJ:2,
+  ST:11,
+}
+
+//for in loop
+// for(let a in obj){
+//   console.log(a)
+//   console.log("Marks of "+a+" are "+obj[a])
+// }
+
+//for of loop
+for(let b of "Khan"){
+  console.log(b)
+}
+//for of loop doesn't work in objects
