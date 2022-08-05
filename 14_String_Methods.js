4a559298bd8d56b8418caff02e55c02d4faa3b53
Shahbaz Khan <72274984+ShahbazCS@users.noreply.github.com>
2022-08-05 16:00:44 +0530

    Create 14_String_Methods.js

---
+++ 14_String_Methods.js
@@ -0,0 +1,19 @@
+let name="KhanSahab"
+console.log(name.length)
+  //name="Kha\nn"
+console.log(name.length)
+
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4)) //4 is not included
+console.log(name.slice(2)) 
+console.log(name.replace("Khan","Bibek"))
+let friend = "Bibek"
+console.log(name.concat(" is a friend of ", friend))
+let friend2="        Bisal    "
+console.log(friend2)
+friend2=friend2.trim()
+console.log(friend2)
+//string is immutable in javascript
+
+
